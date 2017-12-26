92f21018778a9b484748c04767d516dd1368f597
ErezBouskila <erez.bouskila@kaltura.com>
2017-12-26 09:35:57 +0200

    added functions

--- bookmarklet.js
+++ bookmarklet.js
@@ -18,23 +18,27 @@ createBookmarklet = () => {
     let inputValue = document.querySelector(".myBookmarkletName").value;
     let textAreaInputValue = document.querySelector(".myBookmarkletCode").value;
     let t = document.createTextNode(inputValue);
+
     li.setAttribute("id", "testLi");
     li.setAttribute("data-code", textAreaInputValue);
     li.appendChild(t);
-    //Todo:: Make a validator function
+
+    validateInput(inputValue, li);
+    clearTextFields();
+
+};
+
+validateInput = (inputValue, li) => {
     if (inputValue === '') {
         alert("You must write something!");
     } else {
         document.querySelector(".bookmarkletList").appendChild(li);
     }
-    //TODO:: Make a clear function
+};
+
+clearTextFields = () => {
     document.querySelector(".myBookmarkletName").value = "";
     document.querySelector(".myBookmarkletCode").value = "";
-
 };
 
-generate = (el, list) => {
-    el.innerHTML = list.reduce((acc, curr) => {
-        return `${acc}<li data-code="${textAreaInputValue}" >${curr.name}</li>`
-    }, '');
-};
+
